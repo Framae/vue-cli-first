086df8e4541f19710792e0e14f5f06d38fc50f2f
Francesco Maestrelli <framae26@gmail.com>
2022-02-21 18:07:24 +0100

    push

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Questo è il titolo</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TitleComponent',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
